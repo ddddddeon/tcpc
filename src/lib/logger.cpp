81c1a7cb5314b32779deacb7892fea3c0214f336
ddddddeon <opfunk@gmail.com>
2022-02-05 23:43:10 -0500

    add clang-format and clang-tidy to makefile

--- src/lib/logger.cpp
+++ src/lib/logger.cpp
@@ -1,38 +1,31 @@
+#include "logger.h"
+
 #include <iostream>
 #include <string>
 
-#include "logger.h"
-
 std::string const levels[3] = {"INFO", "WARN", "ERROR"};
 
-void Logger::Info(std::string str)
-{
-    level = INFO;
-    Logger::Log(str);
+void Logger::Info(std::string str) {
+  level = INFO;
+  Logger::Log(str);
 }
 
-void Logger::Warn(std::string str)
-{
-    level = WARN;
-    Logger::Log(str);
+void Logger::Warn(std::string str) {
+  level = WARN;
+  Logger::Log(str);
 }
 
-void Logger::Error(std::string str)
-{
-    level = ERROR;
-    Logger::Log(str);
+void Logger::Error(std::string str) {
+  level = ERROR;
+  Logger::Log(str);
 }
 
-void Logger::Log(std::string str)
-{
-    std::string message = "[" + levels[level] + "]" + " " + str;
+void Logger::Log(std::string str) {
+  std::string message = "[" + levels[level] + "]" + " " + str;
 
-    if (str[str.length() - 1] == '\n')
-    {
-        std::cout << message;
-    }
-    else
-    {
-        std::cout << message << std::endl;
-    }
+  if (str[str.length() - 1] == '\n') {
+    std::cout << message;
+  } else {
+    std::cout << message << std::endl;
+  }
 }
--- src/lib/logger.h
+++ src/lib/logger.h
@@ -7,20 +7,16 @@
 #define WARN 1
 #define ERROR 2
 
-class Logger
-{
-public:
-    int level;
+class Logger {
+ public:
+  int level;
 
-    Logger()
-    {
-        level = 0;
-    }
+  Logger() { level = 0; }
 
-    void Info(std::string str);
-    void Warn(std::string str);
-    void Error(std::string str);
-    void Log(std::string str);
+  void Info(std::string str);
+  void Warn(std::string str);
+  void Error(std::string str);
+  void Log(std::string str);
 };
 
 #endif /* !LOGGER_H */
